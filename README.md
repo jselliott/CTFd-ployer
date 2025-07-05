84cd374ac5dc4b7686d291f66524c1c07f50bcaf
Jacob Elliott <jacob@jseis.me>
2025-07-05 09:16:34 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# CTFd-ployer
-A low cost per-player instancing system for CTFd
+# CTFd-Ployer
+A collection of docker-compose services for deploying per-player CTF challenge instances on a large GCP instance. This is designed to happen in a private VPC so that CTFd can communicate with the service on the backend as needed. The system was designed for small clubs and organizations that want the flexibility of having per-player instances without the overhead of using Kubernetes. During the 2025 US Cyber Open, this system allowed for over 1,300 players to launch challenges on demand at a cost of around $15 per day using a e2-highmem-8 (8 vCPUs, 64 GB Memory) in GCP as the challenge host.
+
+### General Workflow
+
+* Player clicks the "Start Instance" button on a CTF challenge on a CTFd instance using the deployer plugin.
+* CTFd send a request to the deployer app asking it to spin up a specific image with a timeout and player ID from CTFd
+* If another challenge is currently deployed for that user, it is shut down
+* The challenge is deployed on a random, available high number port
+* A new entry is mapped in nginx with a random subdomain
+* The domain name is returned to CTFd, which displays it for the user and also shows a countdown
+* Additional endpoints for /status, etc can be pinged to return instance status for a player to CTFd
+* When a challenge times out or the player clicks the Stop button, the container is stopped and removed and the nginx proxy removes the path
+
+### TCP Connect Challenges
+
+For TCP connection challenges for pwn/crypto/misc/etc, the system will skip the nginx proxy and just return the raw port to the service for players to connect to.
+
+## Installation
+
+* Clone this repo
+
+```git clone https://github.com/jselliott/CTFd-ployer.git```
+
+* Update the base domain in docker-compose.yaml
+* Update the SSL certs that are mounted in the nginx container to a valid wildcard certificate that will allow for random subdomains for instances.
+* Build and run the services
+
+```
+docker-compose build
+docker-compose up -d
+```
+
+* Consider pre-pulling images for challenges so they are on-hand when needed and don't slow down launch times.
+* Install the [CTFd-Ployer Plugin](https://github.com/jselliott/CTFd-ployer-Plugin) on your CTFd instance and configure it.
+
+## !!! SECURITY CONSIDERATIONS !!!
+
+This system allows the launcher service to start and stop Docker containers. This means that it can potentially be used to exploit your system if exposed to the internet and an attacker can launch arbitrary containers. **ONLY** run this inside a controlled VPC and set up firewall rules so that port 443 is exposed to the internet as well as high ephemeral ports for TCP challenges. Port 8000 should **ONLY** allow communication from your CTFd host. Additionally, the service account for your instance should only have very basic read registry permissions to pull images needed for challenges. I'm not responsible if you get hacked!
---
+++ launcher/main.py
@@ -0,0 +1,239 @@
+import os
+import string
+import random
+import docker
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from pathlib import Path
+import socket
+import time
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+
+BASE_DOMAIN = os.getenv("BASE_URL","web.ctf.uscybergames.com")
+
+app = FastAPI()
+client = docker.from_env()
+NGINX_CONF_DIR = "/mnt/nginx/conf.d"
+
+class LaunchRequest(BaseModel):
+    image: str
+    port: str
+    challenge_id: str
+    player_id: str
+    expires: str
+    flag: str = ""
+    type: str = "web"
+
+class StopRequest(BaseModel):
+    player_id: str
+    challenge_id: str
+
+class StopContainerRequest(BaseModel):
+    container_id: str
+
+class StatusRequest(BaseModel):
+    player_id: str
+    challenge_id: str
+
+def random_subdomain():
+    return ''.join(random.choices(string.ascii_lowercase, k=8))
+
+
+def get_free_port():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('', 0))
+        return s.getsockname()[1]
+
+@app.get("/")
+def health():
+    return {"healthy":"OK"}
+
+@app.post("/launch")
+def launch_challenge(req: LaunchRequest):
+    sub = random_subdomain()
+    port = get_free_port()
+
+    container_name = f"{req.player_id}_{sub}"
+    fqdn = f"{sub}.{BASE_DOMAIN}"
+
+    if req.flag != "":
+
+        path = f"/tmp/flags/{container_name}.flag"
+
+        with open(path, "w") as f:
+            f.write(req.flag)
+
+        volumes={path: {'bind': '/flag.txt', 'mode': 'ro'}}
+
+    else:
+        volumes = {}
+
+    try:
+        container = client.containers.run(
+            req.image,
+            name=container_name,
+            ports={"%s/tcp" % req.port: port},
+            detach=True,
+            volumes=volumes,
+            environment={"FQDN":fqdn},
+            labels={"challenge_container":"true","ctf_player": req.player_id, "ctf_subdomain": sub, "started_at":str(int(time.time())),"expires":req.expires, "ctf_challenge": req.challenge_id}
+        )
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail={"error":str(e)})
+
+    if req.type == "web":
+        # Write NGINX route
+        nginx_conf = f"""
+server {{
+    listen 80;
+    server_name {fqdn};
+    return 301 https://$host$request_uri;
+}}
+
+server {{
+    listen 443 ssl;
+    server_name {fqdn};
+
+    ssl_certificate     /etc/ssl/certs/fullchain.pem;
+    ssl_certificate_key /etc/ssl/private/privkey.pem;
+
+    location / {{
+        
+        if ($is_bad_agent) {{
+            return 302 {BASE_DOMAIN}/rules.html;
+        }}
+
+        limit_req zone=perip burst=10;
+        proxy_pass http://172.17.0.1:{port};
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+    }}
+}}
+    """
+
+        conf_path = Path(NGINX_CONF_DIR) / f"{fqdn}.conf"
+        conf_path.write_text(nginx_conf)
+        
+        nginx = client.containers.get("uscg-nginx")
+        output = nginx.exec_run("nginx -s reload")
+
+        return {"url": f"https://{fqdn}", "container": container.id}
+    else:
+        return {"url":f'nc challenge.ctf.uscybergames.com {port}', "container": container.id}
+
+@app.post("/status")
+def player_status(req: StatusRequest):
+    containers = client.containers.list(
+        filters={"label": [f"ctf_player={req.player_id}",
+                           f"ctf_challenge={req.challenge_id}"]}
+    )
+    if not containers:
+        return []
+
+    clist = []
+    for c in containers:
+        expires = c.labels.get("ctf_player")
+        clist.append({"expires":expires})
+    return clist
+
+@app.post("/stop")
+def stop_challenge(req: StopRequest):
+
+    containers = client.containers.list(
+        filters={"label": [f"ctf_player={req.player_id}",
+                           f"ctf_challenge={req.challenge_id}"]}
+    )
+    if not containers:
+        raise HTTPException(status_code=404, detail={"error":"Container not found"})
+
+    stopped = []
+    for c in containers:
+        try:
+
+            sub = c.labels.get("ctf_subdomain",None)
+
+            c.stop()
+            c.remove()
+            stopped.append(c.name)
+
+            # If a dynamic flag was added, remove it after removing the container
+            flag_path = Path("/tmp/flags") / f"{c.name}.flag"
+
+            if flag_path.exists():
+                flag_path.unlink()
+
+            # Remove nginx config for subdomain and reload
+            if sub is not None:
+                fqdn = f"{sub}.{BASE_DOMAIN}"
+                conf_path = Path(NGINX_CONF_DIR) / f"{fqdn}.conf"
+                conf_path.unlink()
+                
+                nginx = client.containers.get("uscg-nginx")
+                output = nginx.exec_run("nginx -s reload")
+
+        #Continue if nginx file isn't found due to stale references or whatever
+        except FileNotFoundError:
+            pass
+        except Exception as e:
+            raise HTTPException(status_code=500, detail={"error":str(e)})
+
+    return {"stopped": stopped}
+
+@app.post("/stop_container")
+def stop_challenge(req: StopContainerRequest):
+
+    container = client.containers.get(req.container_id)
+
+    if not container:
+        raise HTTPException(status_code=404, detail={"error":"Container not found"})
+
+    stopped = []
+
+    try:
+
+        sub = container.labels.get("ctf_subdomain",None)
+
+        container.stop()
+        container.remove()
+        stopped.append(container.name)
+
+        # Remove nginx config for subdomain and reload
+        if sub is not None:
+            fqdn = f"{sub}.{BASE_DOMAIN}"
+            conf_path = Path(NGINX_CONF_DIR) / f"{fqdn}.conf"
+            conf_path.unlink()
+            
+            nginx = client.containers.get("uscg-nginx")
+            output = nginx.exec_run("nginx -s reload")
+
+    #Continue if nginx file isn't found due to stale references or whatever
+    except FileNotFoundError:
+        pass
+    except Exception as e:
+        raise HTTPException(status_code=500, detail={"error":str(e)})
+
+    return {"stopped": stopped}
+
+@app.get("/containers")
+def list_containers():
+
+    containers = client.containers.list(filters={"label": f"challenge_container=true"})
+
+    output = []
+
+    for c in containers:
+        labels = c.labels
+        output.append({"player_id":labels.get("ctf_player","0"),
+                       "challenge_id":labels.get("ctf_challenge","0"),
+                       "started_at":labels.get("started_at","0"),
+                       "expires":labels.get("expires","0")})
+    
+    return output
+
+@app.get("/count")
+def count_containers():
+    C = client.containers.list(filters={"label": f"challenge_container=true"})
+    return {"count":len(C)}
